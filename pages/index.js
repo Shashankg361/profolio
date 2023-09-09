d25fd381e0019e0e54f42f8ae0c1e982604004d7
shashankg361 <shashankg361@gmail.com>
2023-09-09 20:38:01 +0530

    error while building the app is solvd

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
   import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
   import { faCopyright } from "@fortawesome/free-regular-svg-icons";
   import { faLinkedin, faTwitter , faGoogle , faInstagram } from "@fortawesome/free-brands-svg-icons";
+  import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -27,7 +28,7 @@ export default function Home() {
               profile photo
             </div>
             <div className="profileName">
-              <h1> Hello! I'm , </h1>
+              <h1> Hello! I;m , </h1>
               <h1>Shashank</h1>
             <div className="profileDescription">description</div>
             </div>
@@ -37,22 +38,22 @@ export default function Home() {
         <div className="projectContainer m-14">
           <ul>
             <li>
-              <div className="card"><h1 className="p-1">Weather site</h1><img src="/Weather site.JPG" alt="Weather site" width="250px" height="150px"></img></div>
+              <div className="card"><h1 className="p-1">Weather site</h1><Image src="/Weather site.JPG" alt="Weather site" width="250px" height="150px" /></div>
             </li>
             <li>
-              <div className="card"><h1 className="py-5">Todo</h1><img src="/TODO Javascrpt.JPG" alt="Todo" width="250px" height="3500px"></img></div>
+              <div className="card"><h1 className="py-5">Todo</h1><Image src="/TODO Javascrpt.JPG" alt="Todo" width="250px" height="3500px" /></div>
             </li>
             <li>
-            <div className="card"><h1 className="p-2">Web calculator</h1><img src="/webCalculator.JPG" alt="Web calculator" width="250px" height="200px"></img></div>
+            <div className="card"><h1 className="p-2">Web calculator</h1><Image src="/webCalculator.JPG" alt="Web calculator" width="250px" height="200px" /></div>
             </li>
             <li>
-              <div className="card"><h1 className="p-2">Communtiy landing page</h1><div className="w-70 h-96 bg-black flex items-center"><img src="/Community.JPG" alt="community page" width="250px" height="700px"></img></div></div>
+              <div className="card"><h1 className="p-2">Communtiy landing page</h1><div className="w-70 h-96 bg-black flex items-center"><Image src="/Community.JPG" alt="community page" width="250px" height="700px" /></div></div>
             </li>
             <li>
-            <div className="card"><h1 className="p-2">Web calculator</h1><img src="/webCalculator.JPG" alt="Web calculator" width="250px" height="200px"></img></div>
+            <div className="card"><h1 className="p-2">Web calculator</h1><Image src="/webCalculator.JPG" alt="Web calculator" width="250px" height="200px" /></div>
             </li>
             <li>
-            <div className="card"><h1 className="p-2">Web calculator</h1><img src="/webCalculator.JPG" alt="Web calculator" width="250px" height="200px"></img></div>
+            <div className="card"><h1 className="p-2">Web calculator</h1><Image src="/webCalculator.JPG" alt="Web calculator" width="250px" height="200px" /></div>
             </li>
           </ul>
         </div>
@@ -62,7 +63,7 @@ export default function Home() {
             <div><text> About Me ! </text></div>
             <div className="w-96">
               <p>
-                Hello! I'm shashank gupta a computer engineer student , pursuing this degree from <a>K.C. college of engineering</a> affiliated by  
+                Hello! I;m shashank gupta a computer engineer student , pursuing this degree from <a>K.C. college of engineering</a> affiliated by  
                 <a> Mumbai university</a> currently I am in final year of my degree . Aspiring developer who works on self project and learn the 
                 about technologies from the internet 
               </p>
@@ -74,21 +75,21 @@ export default function Home() {
           <div>
             <div className="text-black text-center text-2xl"><h1>Languages</h1></div>
             <div className="Languages">
-              <div className="flex items-center p-5"><img src="https://www.ankitweblogic.com/javascript/js_img/javascript.png" alt="Javascript Icon" width="85px" height="85px"></img><span><h1>Javascript</h1></span></div>
-              <div className="flex items-center p-5"><img src="https://www.pngall.com/wp-content/uploads/2016/05/Java-Free-Download-PNG.png" alt="Java Icon" width="85px" height="85px" className="p-3"></img><span className="p-2"><h1>Java</h1></span></div>
-              <div className="flex items-center p-5"><img src="https://www.freeiconspng.com/uploads/c--logo-icon-0.png" alt="C++ Icon" width="85px" height="85px" className="p-3"></img><span className="p-2"><h1>C++</h1></span></div>
-              <div className="flex items-center p-5"><img src="https://cdn-icons-png.flaticon.com/512/732/732212.png" alt="Html Icon" width="85px" height="85px" className="p-3"></img><span className="p-2"><h1>HTML</h1></span></div>
-              <div className="flex items-center p-5"><img src="https://www.pngitem.com/pimgs/m/198-1985012_transparent-css3-logo-png-css-logo-transparent-background.png" alt="Css Icon" width="85px" height="85px" className="p-3"></img><span className="p-2"><h1>CSS</h1></span></div>
+              <div className="flex items-center p-5"><Image src="https://www.ankitweblogic.com/javascript/js_Image/javascript.png" alt="Javascript Icon" width="85px" height="85px" /><span><h1>Javascript</h1></span></div>
+              <div className="flex items-center p-5"><Image src="https://www.pngall.com/wp-content/uploads/2016/05/Java-Free-Download-PNG.png" alt="Java Icon" width="85px" height="85px" className="p-3" /><span className="p-2"><h1>Java</h1></span></div>
+              <div className="flex items-center p-5"><Image src="https://www.freeiconspng.com/uploads/c--logo-icon-0.png" alt="C++ Icon" width="85px" height="85px" className="p-3" /><span className="p-2" ><h1>C++</h1></span></div>
+              <div className="flex items-center p-5"><Image src="https://cdn-icons-png.flaticon.com/512/732/732212.png" alt="Html Icon" width="85px" height="85px" className="p-3" /><span className="p-2" ><h1>HTML</h1></span></div>
+              <div className="flex items-center p-5"><Image src="https://www.pngitem.com/pImages/m/198-1985012_transparent-css3-logo-png-css-logo-transparent-background.png" alt="Css Icon" width="85px" height="85px" className="p-3" /><span className="p-2"><h1>CSS</h1></span></div>
             </div>
           </div>
           <div>
             <div className="text-black text-center text-2xl"><h1>Frameworks and Technologies</h1></div>
             <div className="technologies">
-                <div className="flex items-center p-5"><img src="https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png" alt="react Icon" width="60px" height="60px"></img><span className="p-2"><h1>React</h1></span></div>
-                <div className="flex items-center p-5"><img src="https://static-00.iconduck.com/assets.00/nextjs-icon-2048x1234-pqycciiu.png" alt="Next.js Icon" width="60px" height="60px"></img><span className="p-2"><h1>Next.js</h1></span></div>
-                <div className="flex items-center p-5"><img src="https://p7.hiclipart.com/preview/747/798/387/mysql-database-web-development-computer-software-dolphin.jpg" alt="MySql Icon" width="60px" height="60px"></img><span className="p-2"><h1>MySql</h1></span></div>
-                <div className="flex items-center p-5"><img src="https://w7.pngwing.com/pngs/293/485/png-transparent-tailwind-css-hd-logo.png" alt="Tailwind Icon" width="60px" height="60px"></img><span className="p-2"><h1>Tailwind</h1></span></div>
-                <div className="flex items-center p-5"><img src="https://miro.medium.com/v2/resize:fit:304/1*BXaq7UZ-QoMH0vF0Znjy0A.jpeg" alt="Firestore Icon" width="60px" height="v"></img><span className="p-2"><h1>Firestore</h1></span></div>
+                <div className="flex items-center p-5"><Image src="https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png" alt="react Icon" width="60px" height="60px" /><span className="p-2" ><h1>React</h1></span></div>
+                <div className="flex items-center p-5"><Image src="https://static-00.iconduck.com/assets.00/nextjs-icon-2048x1234-pqycciiu.png" alt="Next.js Icon" width="60px" height="60px" /><span className="p-2"><h1>Next.js</h1></span></div>
+                <div className="flex items-center p-5"><Image src="https://p7.hiclipart.com/preview/747/798/387/mysql-database-web-development-computer-software-dolphin.jpg" alt="MySql Icon" width="60px" height="60px" /><span className="p-2"><h1>MySql</h1></span></div>
+                <div className="flex items-center p-5"><Image src="https://w7.pngwing.com/pngs/293/485/png-transparent-tailwind-css-hd-logo.png" alt="Tailwind Icon" width="60px" height="60px" /><span className="p-2" ><h1>Tailwind</h1></span></div>
+                <div className="flex items-center p-5"><Image src="https://miro.medium.com/v2/resize:fit:304/1*BXaq7UZ-QoMH0vF0Znjy0A.jpeg" alt="Firestore Icon" width="60px" height="v" /><span className="p-2"><h1>Firestore</h1></span></div>
             </div>  
           </div>
         </div>
